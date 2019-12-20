fa7084e6f9fc01e43cbd85374e4e26486be41a66
Matthieu Vidal <matthieuvdl93@gmail.com>
2019-12-20 03:44:17 +0100

    All Covers + Character Infos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page implements OnInit {
-  maxProfile = 5;
+  maxProfile = 6;
   profileId = 0;
   age = 0;
   name = "test";
   ville = "test";
+  avatar = "";
   genreId1 = "";
   genreId2 = "";
   genreId3 = "";
@@ -36,80 +37,86 @@ export class Tab2Page implements OnInit {
       if(x == 0)
       {
         this.name = "Jean";
-        this.age = 18;
+        this.avatar = "jean.jpg";
+        this.age = 24;
         this.ville = "Paris";
         this.genreId1 = "rock";
         this.genreId2 = "electro";
         this.genreId3 = "jazz";
-        this.albumId1 = "Test";
-        this.albumId2 = "Test3";
-        this.albumId3 = "Test2";
-        this.albumId4 = "Test1";
+        this.albumId1 = "daftpunk";
+        this.albumId2 = "muse";
+        this.albumId3 = "ram";
+        this.albumId4 = "lolozouai";
       }
       if(x == 1)
       {
         this.name = "Alexis";
+        this.avatar = "alexis.jpg";
         this.age = 24;
         this.ville = "Lyon";
         this.genreId1 = "rock";
         this.genreId2 = "metal";
         this.genreId3 = "rnb";
-        this.albumId1 = "Test";
-        this.albumId2 = "Test3";
-        this.albumId3 = "Test2";
-        this.albumId4 = "Test1";
+        this.albumId1 = "nirvana";
+        this.albumId2 = "toxicity";
+        this.albumId3 = "drake";
+        this.albumId4 = "linkinpark";
       }
       if(x == 2)
       {
-        this.name = "Matthieu";
+        this.name = "Axel";
+        this.avatar = "axel.jpg";
         this.age = 20;
-        this.ville = "Villejuif";
+        this.ville = "Villiers-sur-Marne";
         this.genreId1 = "jazz";
         this.genreId2 = "funk";
         this.genreId3 = "rnb";
-        this.albumId1 = "Test";
-        this.albumId2 = "Test3";
-        this.albumId3 = "Test2";
-        this.albumId4 = "Test1";
+        this.albumId1 = "kindofblue";
+        this.albumId2 = "givemethenight";
+        this.albumId3 = "rhfactor";
+        this.albumId4 = "coltrane";
       }
       if(x == 3)
       {
         this.name = "Magalie";
+        this.avatar = "magalie.jpg";
         this.age = 31;
         this.ville = "St-Pierre-du-Perray";
         this.genreId1 = "pop";
         this.genreId2 = "electro";
         this.genreId3 = "disco";
-        this.albumId1 = "Test";
-        this.albumId2 = "Test3";
-        this.albumId3 = "Test2";
-        this.albumId4 = "Test1";
+        this.albumId1 = "koolandthegang";
+        this.albumId2 = "brunomars";
+        this.albumId3 = "ewf";
+        this.albumId4 = "faces";
       }
       if(x == 4)
       {
         this.name = "Sarah";
         this.age = 25;
+        this.avatar = "sarah.jpg";
         this.ville = "Villeurbanne"
         this.genreId1 = "rnb";
         this.genreId2 = "jazz";
         this.genreId3 = "electro";
-        this.albumId1 = "Test";
-        this.albumId2 = "Test3";
-        this.albumId3 = "Test2";
-        this.albumId4 = "Test1";
+        this.albumId1 = "billevans";
+        this.albumId2 = "rihanna";
+        this.albumId3 = "biscuit";
+        this.albumId4 = "ram";
       }
       if(x == 5)
       {
         this.name = "Killian";
-        this.age = 22;
+        this.age = 33;
+        this.avatar = "killian.jpg";
         this.ville = "Marly-Gomont";
         this.genreId1 = "rock";
         this.genreId2 = "pop";
         this.genreId3 = "reggae";
-        this.albumId1 = "Test";
-        this.albumId2 = "Test3";
-        this.albumId3 = "Test2";
-        this.albumId4 = "Test1";
+        this.albumId1 = "mika";
+        this.albumId2 = "nirvana";
+        this.albumId3 = "acdc";
+        this.albumId4 = "rolling";
       }
     }
 
